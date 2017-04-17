c760820102a2f597e92b1c279b3c108f750759ad
lipeishang <1315789454@qq.com>
2017-04-17 17:31:48 +0800

    the use of moment

--- get-moment-time.js
+++ get-moment-time.js
@@ -4,10 +4,16 @@
 const moment = require('moment');
 
 function getMoment() {
-   var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var time = moment().format('YYYY-MM-DD HH:mm:ss');
     console.log(time);
 
-   var duringTime = moment("20111031", "YYYYMMDD").fromNow();
+    var time1 = moment().add(9,'hours').format('HH:mm:ss');  //2014年10月01日 //2 years ago
+    console.log(time1);
+
+    var day = moment("1995-12-25").format('d');
+    console.log(day);
+
+   var duringTime = moment("20150417", "YYYYMMDD").fromNow();
     console.log(duringTime);
 }
 
